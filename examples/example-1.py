be9315a31be3d68306d7ab033ffd446dd3ba06fb
Thomas Brunko <tsbo@gft.com>
2025-02-11 09:56:59 +0100

    Added linebreak at fileend.

--- examples/example-1.py
+++ examples/example-1.py
@@ -14,6 +14,7 @@ curl_handler = sdodtools.UrllibCurl.Handler.CurlHandler()
 # Create an opener using the custom handler
 opener = urllib.request.build_opener(curl_handler)
 
+opener.add_handler(urllib.request.ProxyHandler({}))
 # Use the opener
 response = opener.open('https://httpbin.org/get')
 print(response.read().decode())
